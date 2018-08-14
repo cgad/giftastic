a7679a44ee76fe6770e627c9a6ce5ceb88856179
cgad <35946778+cgad@users.noreply.github.com>
2018-08-13 21:09:21 -0600

    bootstrap styling, pause and animate functionality

--- assets/app.js
+++ assets/app.js
@@ -1,38 +1,50 @@
 $(document).ready(function() {
 
-    // already 3 buttons on page load
-    var search = ["mushroom","donut","cheeseburger"];
+    // already 5 food buttons on page load
+    var search = ["mushroom","donut","cheeseburger","pizza","sushi"];
     for (var i = 0; i < search.length; i++) {
-        // var queryURL = "https://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=RC5qajcnW04a0dwjbL8h2nVGrofeeFqg&q=" + search[i];
         var newButton = $("<button class='item'>").text(search[i]);
         newButton.attr("data-food", search[i]);
-        $("#searchButtons").append(newButton);
+        $("#search-buttons").append(newButton);
     }
-    
-    // for user input
-    // var userQuery = "https://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=RC5qajcnW04a0dwjbL8h2nVGrofeeFqg&q=";
 
+    // click event for submit button: save user input, clear field, add new button
     $("#submit").on("click", function(event) {
         event.preventDefault();
         var userSearch = $("#add").val().trim().toLowerCase();
         $("#add").val("");
         var newButton = $("<button class='item'>").text(userSearch);
         newButton.attr("data-food", userSearch);
-        $("#searchButtons").append(newButton);           
+        $("#search-buttons").append(newButton);           
     })
 
-    // BUG: only works for first item button
-    // FIX: CHANGE FROM ID TO CLASS b/c technically only supposed to have 1 id so it was hooking onto first 1
-    // event delegation to get it to watch things added after initial html load (dynamically added)
-    // so target element on page that wraps around it that is there from load
-    $("#searchButtons").on("click", ".item", function(event) {
+    // click event for food buttons: save button food, create query URL, call ajax, create new div to hold rating and gif image, create p for rating, img for gif image, give img still and animated (gif) values, add to page
+    $("#search-buttons").on("click", ".item", function(event) {
         var food = $(this).attr("data-food");
         var userQuery = "https://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=RC5qajcnW04a0dwjbL8h2nVGrofeeFqg&q=" + food;
-        console.log(food);
+
         $.ajax({url: userQuery, method: "GET"})
          .then(function(response) {
-           console.log(response);
-         })
+            var results = response.data;
+            for (var i = 0; i < 10; i++) {
+                var foodDiv = $("<div class='food-div'>");
+                var p = $("<p>").text("Rating: " + results[i].rating);
+                var foodGif = $("<img>").attr("src", results[i].images.fixed_height.url).attr("data-gif", results[i].images.fixed_height.url).attr("data-still", results[i].images.fixed_height_still.url).attr("data-state", "gif");
+
+                foodDiv.append(p, foodGif);
+                $("#search-results").prepend(foodDiv);
+            }
+
+            // click event for individual gifs: new variable to store current data-state, change source to either still or gif state depending on current state
+            $(".food-div").on("click", "img", function(event) {
+                var state = $(this).attr("data-state");
+                if (state == "gif") {
+                    $(this).attr("src", $(this).attr("data-still")).attr("data-state", "still");
+                } else {
+                    $(this).attr("src", $(this).attr("data-gif")).attr("data-state", "gif");
+                }
+            }) 
+        })
     })
 
 })
